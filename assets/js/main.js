4070fde8ce02f1b6f3c576fa5d77752757d68635
twenty-top <jamesbond1234501@gmail.com>
2025-11-27 17:59:31 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,7 +75,7 @@ if(form){
   bd.innerHTML = `
 <div class="modal">
 <h3>Policy Notice</h3>
-<p>Are you accepting our policy to play in Platin?</p>
+<p>Are you accepting our policy to play in 24casino?</p>
 <div style="display:flex;gap:10px;flex-wrap:wrap">
 <button class="btn" id="age-yes">Yes, Accept</button>
 <button class="btn ghost" id="age-no">Close</button>
